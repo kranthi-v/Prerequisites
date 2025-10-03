e1b7b08d8c3d9aff8052abd23783b52f68a3a79b
kranthi-v <krkranthikumar2003@gmail.com>
2025-10-03 15:21:56 +0530

    first

---
+++ check number positive or neagtive.py
@@ -0,0 +1,8 @@
+N = int(input("Enter A number: "))
+
+if N > 0:
+    print(f'{N} is a positive number')
+elif N < 0:
+    print(f'{N} is a negative number')
+else:
+    print(f'{N} is zero')
---
+++ even or odd.py
@@ -0,0 +1,6 @@
+N = int(input("Enter a integer: "))
+
+if N % 2 == 0:
+    print(f'{N} is an Even Number')
+else:
+    print(f'{N} is AN Odd Number')
