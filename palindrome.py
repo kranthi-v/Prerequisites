8120491c8c53ab2edcad4f8e676c04862122c6d3
kranthi-v <krkranthikumar2003@gmail.com>
2025-10-05 12:13:34 +0530

    palindrome

---
+++ palindrome.py
@@ -0,0 +1,11 @@
+N = int(input("Enter a number: "))
+temp = N
+rev = 0
+while temp > 0:
+    rem = temp % 10
+    rev = (rev * 10) + rem
+    temp//=10
+if N == rev:
+    print(f'{N} is a palindrome')
+else:
+    print(f'{N} is not a palindrome')
